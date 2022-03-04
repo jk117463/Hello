45fa19efc9f41883be8dab9c5540507ee5656646
jk117463 <92452727+jk117463@users.noreply.github.com>
2022-03-04 09:25:16 +0100

    random

--- hello.py
+++ hello.py
@@ -1,4 +1,10 @@
 msg = "HEllo World"
+print (msg.capitalize())
+hello = " Hi I am"
+name ="Jathin"
+greet = hello + name
+print(greet, name)
+
 #x=int(input("Type Anything:"))
 #print(5*x)
 # """ if "a" > "b":
