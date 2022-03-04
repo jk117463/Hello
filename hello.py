648fd3e5a44e0a2d733a2a3f36f824670bc473fc
jk117463 <92452727+jk117463@users.noreply.github.com>
2022-03-04 09:12:55 +0100

    VS code

---
+++ hello.py
@@ -0,0 +1,27 @@
+# msg = "HEllo World"
+# print (msg.capitalize())
+# hello = " Hi I am"
+#name ="Jathin"
+#greet = hello + name
+#print(greet, name)
+#
+#x=int(input("Type Anything:"))
+#print(5*x)
+# """ if "a" > "b":
+#     print("Yes")
+# elif "b" > "a": 
+#     print("No")
+# else:
+#     print("No2") """
+# i=0
+# while(i<5):
+#     print(i)
+#     i=i+1
+#for loop
+sum=0
+for i in range(1,10,2):
+    sum+=i
+    print(sum)
+
+
+
